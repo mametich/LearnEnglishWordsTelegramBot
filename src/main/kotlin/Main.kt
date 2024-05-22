23f82fe8869764f2815ef84b8d5cb44493d274be
Rustem <mametich@rambler.ru>
2024-05-22 22:50:14 +0300

    STEP_2

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,12 +2,27 @@ import java.io.File
 
 fun main() {
 
+    val dictionary = mutableListOf<Word>()
+
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
 
     val listOfWords = wordsFile.readLines()
 
-    listOfWords.forEach {
+    for (line in listOfWords) {
+        val split = line.split("|")
+        val word = Word(original = split[0], translate = split[1])
+        dictionary.add(word)
+
+    }
+
+    dictionary.forEach {
         println(it)
     }
 }
+
+data class Word(
+    var original: String,
+    var translate: String,
+    var correctAnswersCount: Int = 0,
+)
