cf67eae1c215cdc1d555a16766d6b62ec2ba0d3a
Rustem <mametich@rambler.ru>
2024-05-28 22:00:03 +0300

    STEP_4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,18 +14,54 @@ fun main() {
         val word = Word(
             original = split[0],
             translate = split[1],
-            correctAnswersCount = split[2].toIntOrNull() ?: 0 )
+            correctAnswersCount = split[2].toIntOrNull() ?: 0
+        )
         dictionary.add(word)
 
     }
 
-    dictionary.forEach {
-        println(it)
+    println("Меню: 1 - Учить слова, 2 – Статистика, 0 – Выход")
+
+    while (true) {
+
+        val numberFromUser = readlnOrNull()?.toIntOrNull() ?: println("Введите цифру")
+        when (numberFromUser) {
+            1 -> {
+                val notLearnedWords = dictionary.filter { it.correctAnswersCount < 3 }
+                if (notLearnedWords.isEmpty()) {
+                    println("Все слова выучены")
+                    break
+                } else {
+                    val listOfOriginalWords = notLearnedWords.map { it.original }
+                    val translatedWords = dictionary.map { it.translate }
+                    val currentOriginalWord = listOfOriginalWords.shuffled().take(1)
+                    println("Как переводится слово: $currentOriginalWord")
+
+                    translatedWords.take(4).shuffled().forEachIndexed { index, word ->
+                        println("${index + 1} - $word")
+                    }
+                }
+            }
+
+            2 -> {
+                val listOfCorrectAnswer = dictionary.filter { it.correctAnswersCount >= 3 }.size
+                val sizeOfWords = dictionary.size
+                val procentOfCorrectAnswer = (listOfCorrectAnswer.toDouble() / sizeOfWords) * 100
+                println(
+                    "Выучено: $listOfCorrectAnswer из $sizeOfWords слов | " +
+                            "${"%.0f".format(procentOfCorrectAnswer)}%"
+                )
+            }
+
+            0 -> break
+            else -> println("Вы ввели не 1 или 2 или 0")
+        }
     }
 }
+
 
 data class Word(
     var original: String,
     var translate: String,
-    var correctAnswersCount: Int? = 0,
+    var correctAnswersCount: Int = 0,
 )
