029f0da4d656e44abcc53fb648416a1766274599
Rustem <mametich@rambler.ru>
2024-06-24 23:02:24 +0300

    STEP_6

--- src/main/kotlin/LearnedWordTrainer.kt
+++ src/main/kotlin/LearnedWordTrainer.kt
@@ -33,14 +33,15 @@ class LearnedWordTrainer(
         val listOfChooseWords = notLearnedWords.shuffled().take(quantityOfWord)
 
         if (listOfChooseWords.size < quantityOfWord) {
-            val listOfLearnedWords =
-                dictionary.filter { it.correctAnswersCount >= countForRemember }.shuffled()
-
-            val showListOfWord =
-                listOfChooseWords + listOfLearnedWords.take(quantityOfWord - listOfChooseWords.size)
-
+            val listOfLearnedWords = dictionary.filter { it.correctAnswersCount >= countForRemember }.shuffled()
+            val showListOfWord = listOfChooseWords + listOfLearnedWords.take(quantityOfWord - listOfChooseWords.size)
             val currentOriginalWord = listOfChooseWords.random()
-
+            question = Question(
+                variants = showListOfWord,
+                correctAnswer = currentOriginalWord
+            )
+        } else {
+            val currentOriginalWord = listOfChooseWords.random()
             question = Question(
                 variants = listOfChooseWords,
                 correctAnswer = currentOriginalWord
@@ -49,40 +50,40 @@ class LearnedWordTrainer(
         return question
     }
 
-        fun checkAnswer(userAnswerIndex: Int?): Boolean {
-            return question?.let {
-                val indexFromWord = it.variants.indexOf(it.correctAnswer)
-                if (indexFromWord == userAnswerIndex) {
-                    it.correctAnswer.correctAnswersCount++
-                    saveDictionary(dictionary)
-                    true
-                } else {
-                    false
-                }
-            } ?: false
-        }
-
-
-        private fun loadDictionary(): List<Word> {
-            val dictionary = mutableListOf<Word>()
-            val wordsFile = File("words.txt")
-
-            wordsFile.readLines().forEach {
-                val splitLine = it.split("|")
-                if (splitLine.size == 3) {
-                    dictionary.add(Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0))
-                }
+    fun checkAnswer(userAnswerIndex: Int?): Boolean {
+        return question?.let {
+            val indexFromWord = it.variants.indexOf(it.correctAnswer)
+            if (indexFromWord == userAnswerIndex) {
+                it.correctAnswer.correctAnswersCount++
+                saveDictionary(dictionary)
+                true
+            } else {
+                false
             }
-            return dictionary
-        }
+        } ?: false
+    }
 
-        private fun saveDictionary(words: List<Word>) {
-            val wordsFile = File("words.txt")
-            wordsFile.writeText("")
-            for (word in words) {
-                wordsFile.appendText("${word.questionWord}|${word.translate}|${word.correctAnswersCount}\n")
+
+    private fun loadDictionary(): List<Word> {
+        val dictionary = mutableListOf<Word>()
+        val wordsFile = File("words.txt")
+
+        wordsFile.readLines().forEach {
+            val splitLine = it.split("|")
+            if (splitLine.size == 3) {
+                dictionary.add(Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0))
             }
+        }
+        return dictionary
+    }
+
+    private fun saveDictionary(words: List<Word>) {
+        val wordsFile = File("words.txt")
+        wordsFile.writeText("")
+        for (word in words) {
+            wordsFile.appendText("${word.questionWord}|${word.translate}|${word.correctAnswersCount}\n")
         }
     }
+}
 
 
