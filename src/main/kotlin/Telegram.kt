cc0de61f101dd45e94b3a80462b29007becd7fec
Rustem <mametich@rambler.ru>
2024-07-12 07:37:28 +0300

    STEP_9

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -6,6 +6,7 @@ fun main(args: Array<String>) {
 
     val telegramBotService = TelegramBotService(botToken)
 
+    var lastUpdateId = 0
     var updateId = 0
     var chatId = 0
     var textMessage = ""
@@ -14,17 +15,24 @@ fun main(args: Array<String>) {
     val messageChatIdRegex: Regex = "\"chat\":\\{\"id\":(.+?),".toRegex()
     val messageTextRegex: Regex = "\"text\":\"(.+?)\"".toRegex()
     val dataRegex: Regex = "\"data\":\"(.+?)\"".toRegex()
+    val updateIdRegex: Regex = "\"update_id\":\"(.+?)\"".toRegex()
 
     while (true) {
         Thread.sleep(2000)
-        val updates: String = telegramBotService.getUpdates(updateId)
+        val updates: String = telegramBotService.getUpdates(lastUpdateId)
         println(updates)
 
-        val startUpdateId = updates.lastIndexOf("update_id")
-        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
-        if (startUpdateId == -1 || endUpdateId == -1) continue
-        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
-        updateId = updateIdString.toInt() + 1
+        val matchUpdateId: MatchResult? = updateIdRegex.find(updates)
+        val groupsUpdateId = matchUpdateId?.groups
+        val updateIdString = groupsUpdateId?.get(1)?.value
+        updateId = updateIdString?.toIntOrNull() ?: continue
+        lastUpdateId = updateId + 1
+
+//        val startUpdateId = updates.lastIndexOf("update_id")
+//        val endUpdateId = updates.lastIndexOf(",\n\"message\"")
+//        if (startUpdateId == -1 || endUpdateId == -1) continue
+//        val updateIdString = updates.substring(startUpdateId + 11, endUpdateId)
+//        updateId = updateIdString.toInt() + 1
 
         val matchResultId: MatchResult? = messageChatIdRegex.find(updates)
         val groupsId = matchResultId?.groups
